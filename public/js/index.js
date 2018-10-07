344f4f36ffdd8b476c01cada186610581220303d
Satyajit Das <das.satya15@gmail.com>
2018-10-08 03:22:23 +0530

    Format timestamp using moment.js

--- public/js/index.js
+++ public/js/index.js
@@ -9,9 +9,12 @@ console.log('Disconnected');
 });
 
 socket.on('newMessage', function (message) {
-	console.log('newMessage', message);
+
+	var formattedTime = moment(message.createdAt).format('h:mm a');
+
+
 	var li = jQuery('<li></li>');
-	li.text(`${message.from}: ${message.text}`);
+	li.text(`${message.from} ${formattedTime}: ${message.text}`);
 
 jQuery('#messages').append(li);
 
@@ -19,17 +22,17 @@ jQuery('#messages').append(li);
 
 socket.on('newLocationMessage', function (message) {
 
+var formattedTime = moment(message.createdAt).format('h:mm a');
+
 	var li = jQuery('<li></li>');
 	var a = jQuery('<a target="_blank">My current location</a>');
 
-li.text(`${message.from}: `);
+li.text(`${message.from} ${formattedTime}: `);
 a.attr('href', message.url);
 li.append(a);
 jQuery('#messages').append(li);
 
 });
-
-
 
 jQuery('#message-form').on('submit', function (e) {
 	e.preventDefault();
